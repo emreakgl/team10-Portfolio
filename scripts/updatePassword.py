1394b5531f0e6e9cbfc2148a0cafaceb3b5cff74
nae3kz1 <162736443+nae3kz1@users.noreply.github.com>
2025-12-02 20:38:12 -0600

    Update updatePassword.py

--- scripts/updatePassword.py
+++ scripts/updatePassword.py
@@ -1,1 +1,19 @@
+import hashlib
+
+
+print("\n--- Update Website Password ---\n")
+new_password = input("Enter NEW password: ")
+
+
+new_hash = hashlib.sha256(new_password.encode()).hexdigest()
+
+
+config_path = "config.js"
+
+with open(config_path, "w") as f:
+    f.write(f'const storedHash = "{new_hash}";\n')
+
+print("\n✔ Password updated successfully!")
+print(f"✔ New SHA-256 Hash: {new_hash}")
+print("✔ config.js updated.\n")
 
